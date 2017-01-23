210753241dbe191c370dc0e2886f7f0d76e46b4d
dhowe <github.utensil@mailhero.io>
2017-01-24 01:18:36 +0800

    removed OSC deps

--- src/run-sampler.sh
+++ src/run-sampler.sh
@@ -4,4 +4,4 @@ cd ../bin/
 
 # use java 1.5
 
-/System/Library/Frameworks/JavaVM.framework/Versions/1.5/Commands/java -Djava.library.path=.. -classpath .:../core.jar:../jsyn.jar:../../OSCInterconnect/oscP5.jar:../../OSCInterconnect/bin ms.MachineSampler
+java -Djava.library.path=.. -classpath .:../core.jar:../jsyn.jar ms.MachineSampler
